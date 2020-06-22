f0a013de91ac0aa8c1f20ccbd11f6ec353778de8
Daniel Neto <danielneto314@gmail.com>
2020-06-22 14:09:43 -0300

    Implementação da exportação dos dados em formato CSV

--- tests/main.py
+++ tests/main.py
@@ -1,7 +1,12 @@
+import numpy as np
 import normalizer as norm
 from load_data import load_csv
 
-raw_data = load_csv('./tests/raw_data.csv')
+base_path = '/home/daniel/Documentos/Projetos/TCC/Normalizador/tests'  # Trocar por caminho relativo do projeto.
+
+raw_data = load_csv(f'{base_path}/raw_data.csv')
 normalized = norm.normalizer(raw_data)
+
+np.savetxt(f'{base_path}/normalized.csv', normalized, fmt='%.8f')
 
 print(normalized)
